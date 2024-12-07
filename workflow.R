9655245da3cbb702b7e5d2db4c1354c3f76a866f
ADNSlab <muntaha.azad1@gmail.com>
2024-12-07 21:55:35 +0300

    Update workflow.R

--- workflow.R
+++ workflow.R
@@ -1,5 +1,5 @@
 set.seed(1234)
-
+source("DE_analysis.R")
 DE_analysis(geo_accession = "GSE36083", # find this from the GEO accession page
             gpl_id = "GPL570", # find this from the GEO accession page [Platform]
             gsms = "010101" # determine from the experiment summary + overall design. Consider 0 for Control, 1 for case
